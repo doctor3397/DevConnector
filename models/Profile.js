30ed1667a06985242e2b767cb38a06ad188d3d9f
Theresa Shen <tshen@architech.ca>
2018-06-06 15:11:54 -0400

    Create the Profile model

---
+++ models/Profile.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  handle: {
+    type: String,
+    require: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  status: {
+    type: String,
+    require: true
+  },
+  skill: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        require: true
+      },
+      company: {
+        type: String,
+        require: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        tyep: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true
+      },
+      degree: {
+        type: String,
+        require: true
+      },
+      fieldofstudy: {
+        type: String,
+        reuquire: true
+      },
+      from: {
+        type: Date,
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        tyep: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  socail: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    dedault: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("Profile", ProfileSchema);
