42fcad296f1b1b5d5441db939058860173a170dd
Theresa Shen <tshen@architech.ca>
2018-06-07 00:27:15 -0400

    Profile inputs validation

--- validation/login.js
+++ validation/login.js
@@ -6,18 +6,18 @@ module.exports = function validateLoginInput(data) {
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
-  if(!validator.isEmail(data.email)) {
-    errors.Email = "Email is invalid";
+  if (!validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
   }
-  if(validator.isEmpty(data.email)) {
-    errors.Email = "Email field is required";
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
-  if(validator.isEmpty(data.password)) {
+  if (validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
 
   return {
     errors: errors,
     isValid: isEmpty(errors)
-  }
-}
+  };
+};
